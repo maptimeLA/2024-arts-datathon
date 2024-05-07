a6d4dd8592c4c7d57b0f9b3f6c066db84fca996c
Nina Kin <matikin9@gmail.com>
2024-05-06 18:41:52 -0700

    show data on map

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,3 +1,4 @@
+const ART_DATA = '/2024-arts-datathon/data/civic-art-data.csv';
 const mapDiv = document.querySelector('#map');
 const map = L.map(mapDiv).setView([34.0622, -118.2437], 10);
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
@@ -6,3 +7,42 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Brows
     maxZoom: 19,
     minZoom: 0
 }).addTo(map);
+
+function parseCSV(file) {
+    return new Promise((resolve, reject) => {
+        Papa.parse(file, {
+            download: true,
+            header: true,
+            complete: function(results) {
+                resolve(results.data);
+            },
+            error: function(err) {
+                reject(err);
+            }
+        });
+    });
+}
+
+function createMarkers(data) {
+    let markerLayer = L.layerGroup();
+
+    data.forEach(item => {
+        let marker = L.marker([item.Latitude, item.Longitude]);
+        marker.addTo(markerLayer);
+    });
+    map.addLayer(markerLayer);
+}
+
+Promise.all([parseCSV(ART_DATA)])
+    .then(results => {        
+        let artworks = results[0];
+        console.log('Parsed ' + artworks.length + ' artworks');
+
+        let filteredArtworks = artworks.filter(function(row) {
+            return row.Latitude != null && row.Longitude != null && row.Show === "TRUE";
+        });
+        console.log('Filtered ' + filteredArtworks.length + ' artworks');
+        
+        createMarkers(filteredArtworks);
+    })
+    .catch(err => console.error(err));

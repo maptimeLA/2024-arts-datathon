4b293f08a02bb8b32e2afaa26799d6bd85c8b00f
Nina Kin <matikin9@gmail.com>
2024-05-06 18:16:23 -0700

    show map and download data

---
+++ src/scripts/map.js
@@ -0,0 +1,8 @@
+const mapDiv = document.querySelector('#map');
+const map = L.map(mapDiv).setView([34.0622, -118.2437], 10);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), {
+    attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19,
+    minZoom: 0
+}).addTo(map);

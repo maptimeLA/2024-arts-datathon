6de4026a520e7a327e73d07776a05dce3e235d30
Nina Kin <matikin9@gmail.com>
2024-05-06 17:58:53 -0700

    set up for github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://maptimela.github.io',
+  base: '2024-arts-datathon',
+})
